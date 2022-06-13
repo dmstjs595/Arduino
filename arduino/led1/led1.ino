0982757877707755e6de00dc0947ea2f133fe992
dmstjs595 <76214070+dmstjs595@users.noreply.github.com>
2022-06-13 13:52:52 +0900

    Update led1.ino

--- arduino/led1/led1.ino
+++ arduino/led1/led1.ino
@@ -2,13 +2,13 @@ int led1 = 12;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(led1, OUTPUT);
+pinMode(led1, OUTPUT);//led1 입력
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(led1, HIGH);
+digitalWrite(led1, HIGH);//켜기
 delay(1000);
-digitalWrite(led1, LOW);
+digitalWrite(led1, LOW);//끄기
 delay(1000);
 }
